4721cb09d7f3095cd5a726b6dfa421cacb786789
Cord <huchzman@web.de>
2015-11-14 23:44:32 +0100

    Upload of skript and result.txt file.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # getting-and-cleaning-data
 Repository for the course project for "Getting and Cleaning Data".
+
+## How the script "run_analysis.R" works
+The script consists of four parts:
+- Unzip the raw data and read the tables into R
+- Merge the tables into data frames separately for the training and test sets
+- Create the first, tidy dataset
+- Create the second dataset with the average of each variable for each activity and each subject
+
+### Read tables into R
+This is pretty straightforward. The raw data is unzipped using the unzip() function and the txt-files are read using the read.table() function.
+
+### Merge the tables
+Column names are created for the X_test / X_train tables from the features.txt file. These names satisfy point 4 of the assignment "Appropriately labels the data set with descriptive variable names."
+
+The tables are merged using the data.frame() function.
+
+### Create the first, tidy dataset
+The training and test datasets are merged using the bind_rows() function, then only variables containing the strings "mean" or "std" in their name are selected and the acitivity variable is modified so that it contains a descriptive names fir the activities (points 1, 2, and 4 of the assignment).
+
+### Create the second dataset
+The first dataset is grouped by the variables activity and subject and the summarise_each() function is used to calculate the averages for each group.
+
+This dataset is written in the file "result.txt".
---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+library(dplyr)
+
+##### Unzip raw data and read tables
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "getdata_projectfiles_UCI HAR Dataset.zip")
+
+unzip("getdata_projectfiles_UCI HAR Dataset.zip")
+
+features <- read.table("UCI HAR Dataset/features.txt", quote="\"", comment.char="")
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", quote="\"", comment.char="")
+subject_train <- read.table("~/Coursera Data Science/Getting And Cleaning Data/Course Project/UCI HAR Dataset/train/subject_train.txt", quote="\"", comment.char="")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt", quote="\"", comment.char="")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", quote="\"", comment.char="")
+subject_test <- read.table("~/Coursera Data Science/Getting And Cleaning Data/Course Project/UCI HAR Dataset/test/subject_test.txt", quote="\"", comment.char="")
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt", quote="\"", comment.char="")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", quote="\"", comment.char="")
+
+##### Create data frames for the train and test datasets; this merges subject data, activity data and the other variables into one frame
+# create dataframe "train"
+names(X_train) <- as.character(features[, 2]) # this labels the dataset with descriptive variable names
+train <- data.frame(subject = subject_train[, 1], activity = y_train[, 1], X_train)
+train <- tbl_df(train)
+
+# create dataframe "test"
+names(X_test) <- as.character(features[, 2]) # this labels the dataset with descriptive variable names
+test <- data.frame(subject = subject_test[, 1], activity = y_test[, 1], X_test)
+test <- tbl_df(test)
+
+##### Create the first data set
+# merge the training and the test sets, extract only the measurements on the mean and standard deviation, use descriptive activity names
+data_set <- 
+  bind_rows(train, test) %>%
+  select(subject, activity, contains("mean"), contains("std")) %>%
+  mutate(activity = activity_labels[activity, 2])
+
+##### Create the sceond data set and write it to a txt-file
+data_set_2 <- 
+  group_by(data_set, activity, subject) %>%
+  summarise_each(funs(mean))
+
+write.table(file = "result.txt", data_set_2, row.names = FALSE)
